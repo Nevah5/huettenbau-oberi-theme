3b066746adaa1482e31c09e3463bb80ed3158b20
Nevah5 <contact@nevah5.dev>
2024-10-15 15:21:33 +0200

    Added style to html

--- functions.php
+++ functions.php
@@ -4,3 +4,10 @@ register_nav_menus(
     'menu-1' => __('Primary Menu', 'my-custom-theme')
   )
 );
+
+function my_custom_theme_enqueue()
+{
+  wp_enqueue_style('my-custom-theme', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'my_custom_theme_enqueue');
--- index.php
+++ index.php
@@ -1,72 +1,51 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<?php get_template_part('header'); ?>
+<div class="site-content">
+  <?php
+  if (have_posts()) :
 
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="profile" href="https://gmpg.org/xfn/11">
-  <?php wp_head(); ?>
-</head>
+    while (have_posts()) :
 
-<body <?php body_class(); ?>>
-  <header class="site-header">
-    <p class="site-title">
-      <a href="<?php echo esc_url(home_url('/')); ?>">
-        <?php bloginfo('name'); ?>
-      </a>
-    </p>
-    <p class="site-description"><?php bloginfo('description'); ?></p>
-  </header><!-- .site-header -->
-  <div class="site-content">
-    <?php
-    if (have_posts()) :
+      the_post();
+  ?>
 
-      while (have_posts()) :
+  <article <?php post_class(); ?>>
 
-        the_post();
-    ?>
-
-        <article <?php post_class(); ?>>
-
-          <header class="entry-header">
-            <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-            <?php
-            wp_nav_menu(array(
-              'theme_location' => 'menu-1',
-            ));
-            ?>
-          </header><!-- .entry-header -->
-
-          <div class="entry-content">
-            <?php the_content(esc_html__('Continue reading &rarr;', 'my-custom-theme')); ?>
-          </div><!-- .entry-content -->
-
-        </article><!-- #post-## -->
-
+    <header class="entry-header">
+      <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
       <?php
-        // If comments are open or we have at least one comment, load up the comment template.
-        if (comments_open() || get_comments_number()) :
-          comments_template();
-        endif;
-      endwhile;
-    else :
-      ?>
-      <article class="no-results">
+          wp_nav_menu(array(
+            'theme_location' => 'menu-1',
+          ));
+          ?>
+    </header><!-- .entry-header -->
 
-        <header class="entry-header">
-          <h1 class="page-title"><?php esc_html_e('Nothing Found', 'my-custom-theme'); ?></h1>
-        </header><!-- .entry-header -->
+    <div class="entry-content">
+      <?php the_content(esc_html__('Continue reading &rarr;', 'my-custom-theme')); ?>
+    </div><!-- .entry-content -->
 
-        <div class="entry-content">
-          <p><?php esc_html_e('It looks like nothing was found at this location.', 'my-custom-theme'); ?></p>
-        </div><!-- .entry-content -->
+  </article><!-- #post-## -->
 
-      </article><!-- .no-results -->
-    <?php
-    endif;
+  <?php
+      // If comments are open or we have at least one comment, load up the comment template.
+      if (comments_open() || get_comments_number()) :
+        comments_template();
+      endif;
+    endwhile;
+  else :
     ?>
-  </div><!-- .site-content -->
-  <?php wp_footer(); ?>
-</body>
+  <article class="no-results">
 
-</html>
+    <header class="entry-header">
+      <h1 class="page-title"><?php esc_html_e('Nothing Found', 'my-custom-theme'); ?></h1>
+    </header><!-- .entry-header -->
+
+    <div class="entry-content">
+      <p><?php esc_html_e('It looks like nothing was found at this location.', 'my-custom-theme'); ?></p>
+    </div><!-- .entry-content -->
+
+  </article><!-- .no-results -->
+  <?php
+  endif;
+  ?>
+</div><!-- .site-content -->
+<?php get_template_part('footer') ?>
