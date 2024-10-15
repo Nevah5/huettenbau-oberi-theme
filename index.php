68f5b77cc4bc11f9766c6a9e6da7f007e50a02d5
Nevah5 <contact@nevah5.dev>
2024-10-15 19:16:15 +0200

    Fixed theme

--- index.php
+++ index.php
@@ -8,22 +8,22 @@
       the_post();
   ?>
 
-      <article <?php post_class(); ?>>
+  <article <?php post_class(); ?>>
 
-        <header class="entry-header">
-          <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-          <?php
+    <header class="entry-header">
+      <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+      <?php
           wp_nav_menu(array(
             'theme_location' => 'menu-1',
           ));
           ?>
-        </header><!-- .entry-header -->
+    </header><!-- .entry-header -->
 
-        <div class="entry-content">
-          <?php the_content(esc_html__('Continue reading &rarr;', 'my-custom-theme')); ?>
-        </div><!-- .entry-content -->
+    <div class="entry-content">
+      <?php the_content(esc_html__('Continue reading &rarr;', 'my-custom-theme')); ?>
+    </div><!-- .entry-content -->
 
-      </article><!-- #post-## -->
+  </article><!-- #post-## -->
 
   <?php
       // If comments are open or we have at least one comment, load up the comment template.
