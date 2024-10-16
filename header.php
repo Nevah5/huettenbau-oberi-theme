6c26b0599b8671fa72652e7ca0becf83a3936a01
Nevah5 <contact@nevah5.dev>
2024-10-16 14:49:32 +0200

    Made logo smaller

--- header.php
+++ header.php
@@ -7,21 +7,23 @@
   <link rel="profile" href="<https://gmpg.org/xfn/11>">
   <?php wp_head(); ?>
 </head>
-<header class="site-header">
-  <p class="site-title">
-    <a href="<?php echo esc_url(home_url('/')); ?>">
-      <?php
-      if (function_exists('the_custom_logo')) {
-        the_custom_logo();
-      } else {
-        bloginfo('name');
-      }
-      ?>
-    </a>
-  </p>
-  <?php
-  wp_nav_menu(array(
-    'theme_location' => 'menu-1',
-  ));
-  ?>
-</header><!-- .site-header -->
+
+<body>
+  <header class="site-header">
+    <p class="site-title">
+      <a href="<?php echo esc_url(home_url('/')); ?>">
+        <?php
+        if (function_exists('the_custom_logo')) {
+          the_custom_logo();
+        } else {
+          bloginfo('name');
+        }
+        ?>
+      </a>
+    </p>
+    <?php
+    wp_nav_menu(array(
+      'theme_location' => 'menu-1',
+    ));
+    ?>
+  </header><!-- .site-header -->
