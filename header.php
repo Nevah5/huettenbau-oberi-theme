69626616eee09507897dd52977012262e89497bd
Nevah5 <contact@nevah5.dev>
2024-10-27 21:12:34 +0100

    Added responsive navbar

--- header.php
+++ header.php
@@ -12,20 +12,26 @@
   <header class="site-header">
     <p class="site-title">
       <?php
-        if (function_exists('the_custom_logo')) {
-          the_custom_logo();
-        } else {
-          ?>
-      <a href="<?php echo esc_url(home_url('/')); ?>">
-        <?= bloginfo('name'); ?>
-      </a>
+      if (function_exists('the_custom_logo')) {
+        the_custom_logo();
+      } else {
+      ?>
+        <a href="<?php echo esc_url(home_url('/')); ?>">
+          <?= bloginfo('name'); ?>
+        </a>
       <?php
-        }
-        ?>
+      }
+      ?>
     </p>
-    <?php
-    wp_nav_menu(array(
-      'theme_location' => 'menu-1',
-    ));
-    ?>
+    <div class="navigation-menu">
+      <input type="checkbox" name="nav-toggle" id="nav-toggle">
+      <label class="nav-toggle-label" for="nav-toggle">
+        <span></span>
+      </label>
+      <?php
+      wp_nav_menu(array(
+        'theme_location' => 'menu-1',
+      ));
+      ?>
+    </div>
   </header><!-- .site-header -->
