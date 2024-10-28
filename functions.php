cdb04bb9ec411ccb5d784df9b95ca48968a629a2
Nevah5 <contact@nevah5.dev>
2024-10-28 12:50:25 +0100

    Replaced my-custom-theme with huettenbau-oberi-theme

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 register_nav_menus(
   array(
-    'menu-1' => __('Primary Menu', 'my-custom-theme')
+    'menu-1' => __('Primary Menu', 'huettenbau-oberi-theme')
   )
 );
 
 function my_custom_theme_enqueue()
 {
-  wp_enqueue_style('my-custom-theme', get_stylesheet_uri());
+  wp_enqueue_style('huettenbau-oberi-theme', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'my_custom_theme_enqueue');
