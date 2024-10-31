37a99c559e0cce73c98e069fc5fe65ca85cf12a4
Nevah5 <contact@nevah5.dev>
2024-10-31 11:39:49 +0100

    Added customized index.php content

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-<!-- empty -->
+<?php get_template_part('header'); ?>
+<div class="content-wrapper">
+  <div class="site-content">
+    <section class="hero-section">
+      <span>Wilkommen beim</span>
+      <h1>Hüttenbau Oberi</h1>
+    </section>
+    <section>
+      <h2>An den Hammer... fertig... nageln!</h2>
+    </section>
+    <section>
+      <h2>Hör mal wer da hämmert und sägt...</h2>
+      <p>Der Hüttenbau-Oberi ermöglicht Kindern ab der 2. Klasse jährlich in der vierten Sommerferienwoche ein
+        erlebnisreiches Lager.</p>
+      <p>Während drei Tagen bauen die Kinder zusammen mit den freiwilligen Leiter*innen bis zu fünf Meter hohe Hütten
+        aus Holz. Nach dem Bau übernachten die Kinder ab Mittwochabend in ihren selbstgeschaffenen Werken. Die Bauphase
+        und die darauffolgenden Tage sind mit vielen Spielen, Bastelaktivitäten und einem aufregenden Ausflug gefüllt.
+      </p>
+      <p>Der Hüttenbau-Oberi wird mit dem gegründeten Verein im Jahr 2014 durch ehrenamtliche Helfer*innen, mit der
+        Unterstützung von Gönner*innen und Sponsor*innen ermöglicht.</p>
+    </section>
+    <section>
+      <h2>Mehr als Hämmern und Sägen</h2>
+      <p>Für das Hüttenbaulager setzen sich die Leiterinnen und Leiter bereits ein Jahr vor dem Lagerstart intensiv mit
+        einem Lagerthema und einem dazu passenden Rahmenprogramm auseinander, um den Kindern ein abwechslungsreiches und
+        spannendes Lagererlebnis zu bieten.
+      </p>
+      <p>
+        Das Rahmenprogamm beinhaltet jedes Jahr Gruppenspiele, Bastelmöglichkeiten, ein Open-Air-Kino-Film und ein
+        Ausflug.</p>
+    </section>
+    <section>
+      <h2>Unterstützung mit Holz und Herz</h2>
+      <p>Wir sind stolz und dankbar, auf die Unterstützung von grosszügigen Gönnerinnen und Gönnern sowie Sponsorinnen
+        und Sponsoren zählen zu dürfen. Mit ihrem finanziellen Beitrag leisten sie einen wertvollen Beitrag zur
+        Realisierung des Ferienlagers.</p>
+      <p>Möchtest auch du uns finanziell oder als Leiter*in unterstützen? Unter “Unterstütze uns” findest du alle
+        Informationen. Herzlichen Dank bereits jetzt für dein Engagement!</p>
+    </section>
+  </div>
+</div>
+<?php get_template_part('footer') ?>
