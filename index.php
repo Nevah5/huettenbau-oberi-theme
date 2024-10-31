aa02ef148d2d19a26b9df7d3da459e9d751b5bfb
Nevah5 <contact@nevah5.dev>
2024-10-31 13:36:56 +0100

    Render section text in template

--- index.php
+++ index.php
@@ -9,41 +9,25 @@
     <h2>
       <?php echo get_theme_mod('huettenbau-oberi-page-section1-title', __('Section Title', 'huettenbau-oberi-theme')); ?>
     </h2>
+    <?php echo get_theme_mod('huettenbau-oberi-page-section1-text', __('<p>Section Text</p>', 'huettenbau-oberi-theme')); ?>
   </section>
   <section>
     <h2>
       <?php echo get_theme_mod('huettenbau-oberi-page-section2-title', __('Section Title', 'huettenbau-oberi-theme')); ?>
     </h2>
-    <p>Der Hüttenbau-Oberi ermöglicht Kindern ab der 2. Klasse jährlich in der vierten Sommerferienwoche ein
-      erlebnisreiches Lager.</p>
-    <p>Während drei Tagen bauen die Kinder zusammen mit den freiwilligen Leiter*innen bis zu fünf Meter hohe Hütten
-      aus Holz. Nach dem Bau übernachten die Kinder ab Mittwochabend in ihren selbstgeschaffenen Werken. Die Bauphase
-      und die darauffolgenden Tage sind mit vielen Spielen, Bastelaktivitäten und einem aufregenden Ausflug gefüllt.
-    </p>
-    <p>Der Hüttenbau-Oberi wird mit dem gegründeten Verein im Jahr 2014 durch ehrenamtliche Helfer*innen, mit der
-      Unterstützung von Gönner*innen und Sponsor*innen ermöglicht.</p>
+    <?php echo get_theme_mod('huettenbau-oberi-page-section1-text', __('<p>Section Text</p>', 'huettenbau-oberi-theme')); ?>
   </section>
   <section>
     <h2>
       <?php echo get_theme_mod('huettenbau-oberi-page-section3-title', __('Section Title', 'huettenbau-oberi-theme')); ?>
     </h2>
-    <p>Für das Hüttenbaulager setzen sich die Leiterinnen und Leiter bereits ein Jahr vor dem Lagerstart intensiv mit
-      einem Lagerthema und einem dazu passenden Rahmenprogramm auseinander, um den Kindern ein abwechslungsreiches und
-      spannendes Lagererlebnis zu bieten.
-    </p>
-    <p>
-      Das Rahmenprogamm beinhaltet jedes Jahr Gruppenspiele, Bastelmöglichkeiten, ein Open-Air-Kino-Film und ein
-      Ausflug.</p>
+    <?php echo get_theme_mod('huettenbau-oberi-page-section1-text', __('<p>Section Text</p>', 'huettenbau-oberi-theme')); ?>
   </section>
   <section>
     <h2>
       <?php echo get_theme_mod('huettenbau-oberi-page-section4-title', __('Section Title', 'huettenbau-oberi-theme')); ?>
     </h2>
-    <p>Wir sind stolz und dankbar, auf die Unterstützung von grosszügigen Gönnerinnen und Gönnern sowie Sponsorinnen
-      und Sponsoren zählen zu dürfen. Mit ihrem finanziellen Beitrag leisten sie einen wertvollen Beitrag zur
-      Realisierung des Ferienlagers.</p>
-    <p>Möchtest auch du uns finanziell oder als Leiter*in unterstützen? Unter “Unterstütze uns” findest du alle
-      Informationen. Herzlichen Dank bereits jetzt für dein Engagement!</p>
+    <?php echo get_theme_mod('huettenbau-oberi-page-section1-text', __('<p>Section Text</p>', 'huettenbau-oberi-theme')); ?>
   </section>
 </main>
 <?php get_template_part('footer') ?>
